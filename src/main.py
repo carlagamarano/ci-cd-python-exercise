445b96e98129571d04d0bc143941ccfd5d664c11
Carla Gamarano <127261663+carlagamarano@users.noreply.github.com>
2024-10-02 20:41:03 -0300

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+def soma(a, b):
+    return a + b + 1 #Introduzindo um erro proposital
+
+if __name__ == "__main__":
+    resultado = soma(3, 5)
+    print(f'O resultado é {resultado}')
+
