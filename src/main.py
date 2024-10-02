f594d7811470eea0eb3bf23d758c509617a8bf10
Carla Gamarano <127261663+carlagamarano@users.noreply.github.com>
2024-10-02 20:47:37 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ import unittest
 
 
 def soma(a, b):
-    return a + b + 1 #Introduzindo um erro proposital
-
+    return a + b  # Corrigindo o erro
+    
 if __name__ == "__main__":
     resultado = soma(3, 5)
     print(f'O resultado é {resultado}')
